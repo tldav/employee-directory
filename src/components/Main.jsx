9f4897f782bcfb67d226bd1cc76331ff8dce2f29
Neverender <tldavis09@yahoo.com>
2020-05-04 21:33:11 -0500

    added primary component to get started

---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import Container from "./Container";
+
+class Main extends Component {
+	state = {};
+	render() {
+		return <Container></Container>;
+	}
+}
+
+export default Main;
